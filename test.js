cbcc76efceaef927b87e5040aa9b7e6cdd543585
Just1-N00B <46449123+Just1-N00B@users.noreply.github.com>
2023-03-07 13:12:38 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,56 +1,56 @@
 function start() {
-// Create textbox
-const input = document.createElement("input");
-input.type = "text";
-input.id = "barcodeInput";
-input.placeholder = "Barcode";
-input.style.position = "fixed";
+    // Create textbox
+    const input = document.createElement("input");
+    input.type = "text";
+    input.id = "barcodeInput";
+    input.placeholder = "Barcode";
+    input.style.position = "fixed";
 
-// Save initial position of text box
-const initialTop = 10;
-const initialLeft = 10;
-input.style.top = initialTop + "px";
-input.style.left = initialLeft + "px";
+    // Save initial position of text box
+    const initialTop = 10;
+    const initialLeft = 10;
+    input.style.top = initialTop + "px";
+    input.style.left = initialLeft + "px";
 
-// Set z-index and background color of text box
-input.style.zIndex = "9999";
-input.style.backgroundColor = "grey";
-document.body.appendChild(input);
+    // Set z-index and background color of text box
+    input.style.zIndex = "9999";
+    input.style.backgroundColor = "grey";
+    document.body.appendChild(input);
 
-// Add event listener to window to listen for scroll event
-window.addEventListener("scroll", function() {
-  // Calculate how far the page has scrolled
-  const scrollTop = window.pageYOffset;
-  const scrollLeft = window.pageXOffset;
+    // Add event listener to window to listen for scroll event
+    window.addEventListener("scroll", function() {
+        // Calculate how far the page has scrolled
+        const scrollTop = window.pageYOffset;
+        const scrollLeft = window.pageXOffset;
 
-  // Set the new position of the text box based on the scroll distance
-  const scale = 0.0; // Set this to the desired percentage of scroll distance
-  input.style.top = (initialTop + scrollTop * scale) + "px";
-  input.style.left = (initialLeft + scrollLeft * scale) + "px";
-});
+        // Set the new position of the text box based on the scroll distance
+        const scale = 0.0; // Set this to the desired percentage of scroll distance
+        input.style.top = (initialTop + scrollTop * scale) + "px";
+        input.style.left = (initialLeft + scrollLeft * scale) + "px";
+    });
 
-// Add event listener to document to listen for key press
-document.addEventListener("keydown", function(event) {
-    // Check if control + / key combination is pressed
-  if (event.ctrlKey && event.keyCode == 191) {
+    // Add event listener to document to listen for key press
+    document.addEventListener("keydown", function(event) {
+        // Check if control + / key combination is pressed
+        if (event.ctrlKey && event.keyCode == 191) {
 
-    const barcode = document.getElementById("barcodeInput").value;
-    // Find active element and set its value to the barcode data
-    const activeElement = document.activeElement;
-    activeElement.value = barcode;
-    // Create a new "keydown" event with the "enter" key code
-  const enterEvent = new KeyboardEvent("keydown", {
-    keyCode: 13,
-    which: 13,
-    charCode: 13,
-    bubbles: true,
-    cancelable: true
-  });
+            const barcode = document.getElementById("barcodeInput").value;
+            // Find active element and set its value to the barcode data
+            const activeElement = document.activeElement;
+            activeElement.value = barcode;
+            // Create a new "keydown" event with the "enter" key code
+            const enterEvent = new KeyboardEvent("keydown", {
+                keyCode: 13,
+                which: 13,
+                charCode: 13,
+                bubbles: true,
+                cancelable: true
+            });
 
-  // Dispatch the event to the active element
-  activeElement.dispatchEvent(enterEvent);
-    // Clear barcode input field
-    document.getElementById("barcodeInput").value = "";
-  }
-});
+            // Dispatch the event to the active element
+            activeElement.dispatchEvent(enterEvent);
+            // Clear barcode input field
+            document.getElementById("barcodeInput").value = "";
+        }
+    });
 };
