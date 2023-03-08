621d48845f0c733feecf7f985452c7be0b4ec113
Just1-N00B <46449123+Just1-N00B@users.noreply.github.com>
2023-03-08 17:49:46 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,56 +1,24 @@
 function start() {
-    // Create textbox
-    const input = document.createElement("input");
-    input.type = "text";
-    input.id = "barcodeInput";
-    input.placeholder = "Barcode";
-    input.style.position = "fixed";
-
-    // Save initial position of text box
-    const initialTop = 10;
-    const initialLeft = 10;
-    input.style.top = initialTop + "px";
-    input.style.left = initialLeft + "px";
-
-    // Set z-index and background color of text box
-    input.style.zIndex = "9999";
-    input.style.backgroundColor = "grey";
-    document.body.appendChild(input);
-
-    // Add event listener to window to listen for scroll event
-    window.addEventListener("scroll", function() {
-        // Calculate how far the page has scrolled
-        const scrollTop = window.pageYOffset;
-        const scrollLeft = window.pageXOffset;
-
-        // Set the new position of the text box based on the scroll distance
-        const scale = 0.0; // Set this to the desired percentage of scroll distance
-        input.style.top = (initialTop + scrollTop * scale) + "px";
-        input.style.left = (initialLeft + scrollLeft * scale) + "px";
-    });
-
-    // Add event listener to document to listen for key press
-    document.addEventListener("keydown", function(event) {
-        // Check if control + / key combination is pressed
-        if (event.ctrlKey && event.keyCode == 191) {
-
-            const barcode = document.getElementById("barcodeInput").value;
-            // Find active element and set its value to the barcode data
-            const activeElement = document.activeElement;
-            activeElement.value = barcode;
-            // Create a new "keydown" event with the "enter" key code
-            const enterEvent = new KeyboardEvent("keydown", {
-                keyCode: 13,
-                which: 13,
-                charCode: 13,
-                bubbles: true,
-                cancelable: true
-            });
-
-            // Dispatch the event to the active element
-            activeElement.dispatchEvent(enterEvent);
-            // Clear barcode input field
-            document.getElementById("barcodeInput").value = "";
-        }
-    });
+   var _0xcb28=["\x69\x6E\x70\x75\x74","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x74\x79\x70\x65","\x74\x65\x78\x74","\x69\x64","\x62\x61\x72\x63\x6F\x64\x65\x49\x6E\x70\x75\x74","\x70\x6C\x61\x63\x65\x68\x6F\x6C\x64\x65\x72","\x42\x61\x72\x63\x6F\x64\x65","\x70\x6F\x73\x69\x74\x69\x6F\x6E","\x73\x74\x79\x6C\x65","\x66\x69\x78\x65\x64","\x74\x6F\x70","\x70\x78","\x6C\x65\x66\x74","\x7A\x49\x6E\x64\x65\x78","\x39\x39\x39\x39","\x62\x61\x63\x6B\x67\x72\x6F\x75\x6E\x64\x43\x6F\x6C\x6F\x72","\x67\x72\x65\x79","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x62\x6F\x64\x79","\x73\x63\x72\x6F\x6C\x6C","\x70\x61\x67\x65\x59\x4F\x66\x66\x73\x65\x74","\x70\x61\x67\x65\x58\x4F\x66\x66\x73\x65\x74","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x6B\x65\x79\x64\x6F\x77\x6E","\x63\x74\x72\x6C\x4B\x65\x79","\x6B\x65\x79\x43\x6F\x64\x65","\x76\x61\x6C\x75\x65","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x61\x63\x74\x69\x76\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x64\x69\x73\x70\x61\x74\x63\x68\x45\x76\x65\x6E\x74",""];
+const input=document[_0xcb28[1]](_0xcb28[0]);
+input[_0xcb28[2]]= _0xcb28[3];input[_0xcb28[4]]= _0xcb28[5];input[_0xcb28[6]]= _0xcb28[7];input[_0xcb28[9]][_0xcb28[8]]= _0xcb28[10];const initialTop=10;
+const initialLeft=10;
+input[_0xcb28[9]][_0xcb28[11]]= initialTop+ _0xcb28[12];input[_0xcb28[9]][_0xcb28[13]]= initialLeft+ _0xcb28[12];input[_0xcb28[9]][_0xcb28[14]]= _0xcb28[15];input[_0xcb28[9]][_0xcb28[16]]= _0xcb28[17];document[_0xcb28[19]][_0xcb28[18]](input);window[_0xcb28[23]](_0xcb28[20],function()
+{
+	const _0xb6f3x4=window[_0xcb28[21]];
+	const _0xb6f3x5=window[_0xcb28[22]];
+	const _0xb6f3x6=0.0;
+	input[_0xcb28[9]][_0xcb28[11]]= (initialTop+ _0xb6f3x4* _0xb6f3x6)+ _0xcb28[12];input[_0xcb28[9]][_0xcb28[13]]= (initialLeft+ _0xb6f3x5* _0xb6f3x6)+ _0xcb28[12]
+};
+);document[_0xcb28[23]](_0xcb28[24],function(_0xb6f3x7)
+{
+	if(_0xb6f3x7[_0xcb28[25]]&& _0xb6f3x7[_0xcb28[26]]== 191)
+	{
+		const _0xb6f3x8=document[_0xcb28[28]](_0xcb28[5])[_0xcb28[27]];
+		const _0xb6f3x9=document[_0xcb28[29]];
+		_0xb6f3x9[_0xcb28[27]]= _0xb6f3x8;const _0xb6f3xa= new KeyboardEvent(_0xcb28[24],{keyCode:13,which:13,charCode:13,bubbles:true,cancelable:true});
+		_0xb6f3x9[_0xcb28[30]](_0xb6f3xa);document[_0xcb28[28]](_0xcb28[5])[_0xcb28[27]]= _0xcb28[31]
+	};
+};
+)
 };
